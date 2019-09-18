2db2dd8567eacb119815e4bd568558ed99164b3b
Momomash <momomash110697@gmail.com>
2019-09-18 17:47:46 +0300

    Add promo slider

---
+++ Gosstroiservice/assets/js/slider-number.js
@@ -0,0 +1,12 @@
+let counter = 0;
+
+const sliderCheck = () => {
+    let newCounter = +$("li.carousel-indicator.active")[0].getAttribute("data-slide-to");
+    if (newCounter !== counter) {
+        let currentSliderNumber = document.getElementById("current_slider");
+        currentSliderNumber.innerHTML = "0" + (newCounter + 1);
+        counter = newCounter;
+    }
+};
+
+setInterval(sliderCheck, 199);
