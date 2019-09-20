f107a57e5a318ea4994e8ab97a554ab292cbbc54
Momomash <momomash110697@gmail.com>
2019-09-20 18:39:30 +0300

    pause about-company

--- Gosstroiservice/assets/js/slider-number.js
+++ Gosstroiservice/assets/js/slider-number.js
@@ -4,7 +4,7 @@ const sliderCheck = () => {
     let newCounter = +$("li.carousel-indicator.active")[0].getAttribute("data-slide-to");
     if (newCounter !== counter) {
         let currentSliderNumber = document.getElementById("current_slider");
-        currentSliderNumber.innerHTML = "0" + (newCounter + 1);
+        currentSliderNumber.innerHTML = "0" + (newCounter + 1 );
         counter = newCounter;
     }
 };
