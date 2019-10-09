60a423473dbe5aad257220428f4fe11131fdbc17
Momomash <momomash110697@gmail.com>
2019-10-09 17:52:31 +0300

    start

---
+++ ADS/assets/js/slider.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('.header_slider').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        // prevArrow: $('.company-slick-prev'),
+        // nextArrow: $('.company-slick-next'),
+        // responsive: [
+        //     {
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1,
+        //         }
+        //     }
+        // ],
+        dots: true
+    });
+});
