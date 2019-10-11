e986e77a9c053d53a2c16afe31ece8f027687674
Momomash <momomash110697@gmail.com>
2019-10-11 15:05:40 +0300

    add comments block

--- ImpulseAgency/assets/js/slider.js
+++ ImpulseAgency/assets/js/slider.js
@@ -3,17 +3,15 @@ $(document).ready(function() {
         slidesToShow: 1,
         autoplay: true,
         autoplaySpeed: 4000,
-        // prevArrow: $('.company-slick-prev'),
-        // nextArrow: $('.company-slick-next'),
-        // responsive: [
-        //     {
-        //         breakpoint: 1024,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             slidesToScroll: 1,
-        //         }
-        //     }
-        // ],
+        dots: true
+
+    });
+});
+$(document).ready(function() {
+    $('.reviews-slider').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 10000,
         dots: true
 
     });
