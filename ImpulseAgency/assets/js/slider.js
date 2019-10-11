5adacfc7304f73c7b3e91fbc1e28c4e6a198296c
Momomash <momomash110697@gmail.com>
2019-10-11 15:27:58 +0300

    fix home page

--- ImpulseAgency/assets/js/slider.js
+++ ImpulseAgency/assets/js/slider.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
         slidesToShow: 1,
         autoplay: true,
         autoplaySpeed: 4000,
-        dots: true
+        dots: true,
+        appendDots: $('.container1')
 
     });
 });
