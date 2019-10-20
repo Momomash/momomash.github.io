3c4e2120f7b479dc88f85ca6b05378840c7fcdd3
Momomash <momomash110697@gmail.com>
2019-10-20 20:29:53 +0300

    fix fullpage in mobile

--- DigitalPlanet/assets/js/slick-slider-number.js
+++ DigitalPlanet/assets/js/slick-slider-number.js
@@ -4,6 +4,7 @@ let slickCounterAboutCompany = 0;
 const wrapper = (counter) => {
     return () => {
         let activeButton = $('.slider_buttons').find("ul.slick-dots").find(".slick-active").find("button")[0];
+        if (!activeButton) return;
         let newCounter = +activeButton.getAttribute("aria-label").split(" ")[0];
         if (newCounter !== counter) {
             let currentSliderNumber = document.getElementById("current_slider");
